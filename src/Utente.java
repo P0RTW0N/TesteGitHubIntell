b544d60a9fbb6a4e2d021c6c78dbd252fbb9c437
P0RTW0N <zpedro321@gmail.com>
2022-10-03 15:47:13 +0100

    utente final

---
+++ src/Utente.java
@@ -0,0 +1,60 @@
+public class Utente {
+
+    private String nome;
+    private String genero;
+    private int idade;
+    private float altura;
+    private int peso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "nome='" + nome + '\'' +
+                ", genero='" + genero + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
+
+}
